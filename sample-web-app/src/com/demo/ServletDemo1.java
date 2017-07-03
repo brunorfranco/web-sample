4c28c1ff7ccbd2782cbdb4e8334e5e165d1d4067
brunorfranco <bruno.rodrigues.franco1@gmail.com>
2017-07-03 19:22:47 +0100

    Creating sample servlet app
    Creating sample servlet app

---
+++ sample-web-app/src/com/demo/ServletDemo1.java
@@ -0,0 +1,33 @@
+package com.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletDemo1 extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1241834440073041296L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello Servlet Get</h1>");
+		out.println("</body>");
+		out.println("</html>");	
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doPost(req, resp);
+	}
+	
+}
